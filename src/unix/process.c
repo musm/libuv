b77db45541b75dfae562b1253d7bda03a41be757
Simon Kornblith <simon@simonster.com>
2014-02-25 23:41:04 -0500

    Fix a couple pre-existing error handling bugs
    (cherry picked from commit 1289302758b0617f8ef49bea0a40f56a94ebda93)

--- src/unix/process.c
+++ src/unix/process.c
@@ -488,6 +488,7 @@ int uv_spawn(uv_loop_t* loop,
   if (pid == -1) {
     err = -errno;
     uv_rwlock_wrunlock(&loop->cloexec_lock);
+    pthread_sigmask(SIG_SETMASK, &sigset, NULL);
     goto error;
   }
 
@@ -520,8 +521,10 @@ int uv_spawn(uv_loop_t* loop,
    * the parent polls the read end until it EOFs or errors with EPIPE.
    */
   err = uv__make_pipe(signal_pipe, 0);
-  if (err)
+  if (err) {
+    pthread_sigmask(SIG_SETMASK, &sigset, NULL);
     goto error;
+  }
 
   /* Acquire write lock to prevent opening new fds in worker threads */
   uv_rwlock_wrlock(&loop->cloexec_lock);
@@ -533,6 +536,7 @@ int uv_spawn(uv_loop_t* loop,
     uv_rwlock_wrunlock(&loop->cloexec_lock);
     uv__close(signal_pipe[0]);
     uv__close(signal_pipe[1]);
+    pthread_sigmask(SIG_SETMASK, &sigset, NULL);
     goto error;
   }
 
@@ -607,8 +611,6 @@ error:
     if (pipes != pipes_storage)
       uv__free(pipes);
   }
-
-  pthread_sigmask(SIG_SETMASK, &sigset, NULL);
 
   return err;
 #endif
