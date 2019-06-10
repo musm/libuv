JuliaAttic/libuv-archive

prevent libuv from forcibly taking away the controlling terminal from existing processs
see https://github.com/libuv/libuv/issues/973

src/unix/tty.c
@@ -87,7 +87,7 @@ int uv_tty_init(uv_loop_t* loop, uv_tty_t* tty, int fd, int readable) {
      * slave device.
      */
     if (uv__tty_is_slave(fd) && ttyname_r(fd, path, sizeof(path)) == 0)
-      r = uv__open_cloexec(path, O_RDWR);
+      r = uv__open_cloexec(path, O_RDWR | O_NOCTTY);
     else
       r = -1;
 

musm/libuv

(cherry picked from commit ac39c2d737ad7427f5bf12cc465ce5c99a65a11b)

src/unix/tty.c
@@ -139,7 +139,7 @@ int uv_tty_init(uv_loop_t* loop, uv_tty_t* tty, int fd, int unused) {
      * slave device.
      */
     if (uv__tty_is_slave(fd) && ttyname_r(fd, path, sizeof(path)) == 0)
-      r = uv__open_cloexec(path, mode);
+      r = uv__open_cloexec(path, mode | O_NOCTTY);
     else
       r = -1;
 