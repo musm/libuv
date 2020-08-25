JuliaAttic/libuv-archive

Fix a couple pre-existing error handling bugs

src/unix/process.c
@@ -354,10 +354,9 @@ int uv_spawn(uv_loop_t* loop,
   if (stdio_count < 3)
     stdio_count = 3;
 
-  err = -ENOMEM;
   pipes = malloc(stdio_count * sizeof(*pipes));
   if (pipes == NULL)
-    goto error;
+    return -ENOMEM;
 
   for (i = 0; i < stdio_count; i++) {
     pipes[i][0] = -1;
@@ -388,6 +387,7 @@ int uv_spawn(uv_loop_t* loop,
   if (pid == -1) {
     err = -errno;
     uv_rwlock_wrunlock(&loop->cloexec_lock);
+    sigprocmask(SIG_SETMASK, &sigoset, NULL);
     goto error;
   }
 
@@ -420,8 +420,10 @@ int uv_spawn(uv_loop_t* loop,
    * the parent polls the read end until it EOFs or errors with EPIPE.
    */
   err = uv__make_pipe(signal_pipe, 0);
-  if (err)
+  if (err) {
+    sigprocmask(SIG_SETMASK, &sigoset, NULL);
     goto error;
+  }
 
   /* Acquire write lock to prevent opening new fds in worker threads */
   uv_rwlock_wrlock(&loop->cloexec_lock);
@@ -433,6 +435,7 @@ int uv_spawn(uv_loop_t* loop,
     uv_rwlock_wrunlock(&loop->cloexec_lock);
     uv__close(signal_pipe[0]);
     uv__close(signal_pipe[1]);
+    sigprocmask(SIG_SETMASK, &sigoset, NULL);
     goto error;
   }
 
@@ -487,7 +490,6 @@ error:
   }
 
   free(pipes);
-  sigprocmask(SIG_SETMASK, &sigoset, NULL);
 
   return err;
 }

musm/libuv

(cherry picked from commit 1289302758b0617f8ef49bea0a40f56a94ebda93)

src/unix/process.c
@@ -492,6 +492,7 @@ int uv_spawn(uv_loop_t* loop,
   if (pid == -1) {
     err = -errno;
     uv_rwlock_wrunlock(&loop->cloexec_lock);
+    pthread_sigmask(SIG_SETMASK, &sigset, NULL);
     goto error;
   }
 
@@ -524,8 +525,10 @@ int uv_spawn(uv_loop_t* loop,
    * the parent polls the read end until it EOFs or errors with EPIPE.
    */
   err = uv__make_pipe(signal_pipe, 0);
-  if (err)
+  if (err) {
+    pthread_sigmask(SIG_SETMASK, &sigset, NULL);
     goto error;
+  }
 
   /* Acquire write lock to prevent opening new fds in worker threads */
   uv_rwlock_wrlock(&loop->cloexec_lock);
@@ -537,6 +540,7 @@ int uv_spawn(uv_loop_t* loop,
     uv_rwlock_wrunlock(&loop->cloexec_lock);
     uv__close(signal_pipe[0]);
     uv__close(signal_pipe[1]);
+    pthread_sigmask(SIG_SETMASK, &sigset, NULL);
     goto error;
   }
 
@@ -611,8 +615,6 @@ error:
     if (pipes != pipes_storage)
       uv__free(pipes);
   }
-
-  pthread_sigmask(SIG_SETMASK, &sigset, NULL);
 
   return err;
 #endif